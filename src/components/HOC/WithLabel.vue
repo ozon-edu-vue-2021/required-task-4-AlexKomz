66a7e3c8a15b1e925ea1ec88d651058b5ed443bb
KVC\komzoltcev <komzoltcev.a@gmail.com>
2021-11-08 11:53:27 +0300

    HOC для добавления лейбла контролам

---
+++ src/components/HOC/WithLabel.vue
@@ -0,0 +1,30 @@
+<template>
+  <label :for="id">
+    <span class="label">{{ label }}</span>
+    <slot />
+  </label>
+</template>
+
+<script>
+export default {
+  name: "WithLabel",
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      default: ``,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.label {
+  position: relative;
+  display: block;
+  margin-bottom: 5px;
+}
+</style>
